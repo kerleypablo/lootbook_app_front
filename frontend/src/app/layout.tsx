535c8e1461edb4979c445c6da8707138c855a364
kerley pablo <kerley.kls@gmail.com>
2026-09-07 17:58:52 -0300

    feat(frontend): add design tokens

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,16 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Acme, Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const acme = Acme({
+  variable: "--font-acme",
   subsets: ["latin"],
+  weight: "400",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${acme.variable} ${openSans.variable} antialiased`}
       >
         {children}
       </body>
